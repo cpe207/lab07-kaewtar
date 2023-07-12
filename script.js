9e0bd71dbb538c83b5117e9e7dbc6c6da56f2d2d
kaewtar <kaewtarcmu@gmail.com>
2023-07-12 15:14:20 +0700

    completed

--- script.js
+++ script.js
@@ -4,3 +4,48 @@ function validateEmail(email) {
   var dotPos = email.lastIndexOf(".");
   return atPos > 0 && dotPos > atPos + 1 && dotPos < email.length - 1;
 }
+const firstNameInput = document.querySelector("#first-name-input");
+const lastNameInput = document.querySelector("#last-name-input");
+const emailInput = document.querySelector("#email-input");
+const passwordInput = document.querySelector("#password-input");
+const submitBtn = document.querySelector("#submit-btn");
+
+firstNameInput.onkeyup = () => {
+  firstNameInput.classList.remove("is-valid"),
+    firstNameInput.classList.remove("is-invalid");
+};
+lastNameInput.onkeyup = () => {
+  lastNameInput.classList.remove("is-valid"),
+    lastNameInput.classList.remove("is-invalid");
+};
+emailInput.onkeyup = () => {
+  emailInput.classList.remove("is-valid"),
+    emailInput.classList.remove("is-invalid");
+};
+passwordInput.onkeyup = () => {
+  passwordInput.classList.remove("is-valid"),
+    passwordInput.classList.remove("is-invalid");
+};
+submitBtn.onclick = () => {
+  isFirstNameOk = !1;
+  isLastNameOk = !1;
+  isEmailOk = !1;
+  isPasswordOk = !1;
+  "" === firstNameInput.value
+    ? firstNameInput.classList.add("is-invalid")
+    : (firstNameInput.classList.add("is-valid"), (isFirstNameOk = !0)),
+    "" === lastNameInput.value
+      ? lastNameInput.classList.add("is-invalid")
+      : (lastNameInput.classList.add("is-valid"), (isLastNameOk = !0)),
+    !1 === validateEmail(emailInput.value)
+      ? emailInput.classList.add("is-invalid")
+      : (emailInput.classList.add("is-valid"), (isEmailOk = !0)),
+    passwordInput.value.length >= 6
+      ? (passwordInput.classList.add("is-valid"), (isPasswordOk = !0))
+      : passwordInput.classList.add("is-invalid"),
+    isFirstNameOk &&
+      isLastNameOk &&
+      isEmailOk &&
+      isPasswordOk &&
+      alert("Registered successfully");
+};
